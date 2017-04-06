b062b9834b3db6b635f73ad8e5b883aba8812435
Charles Peters <bkrubnzi@frankencloud.net>
2017-04-06 15:41:52 -0500

    Starting from scratch

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import os
+import cf_deployment_tracker
 
 from library._01_root import app
+
+# Emit Bluemix deployment event
+cf_deployment_tracker.track()
 
 
 if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.getenv('PORT', 8080))
     app.run(host=host, port=port)
