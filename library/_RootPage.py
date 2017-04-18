4a014ac8a24f80c4c8cd49d1fc6f9e6cfc8ab455
Charles Peters <bkrubnzi@frankencloud.net>
2017-04-18 16:47:01 -0500

    Added Cloudant DB functionality

--- library/_RootPage.py
+++ library/_RootPage.py
@@ -1,9 +1,14 @@
+import os
 from flask import render_template
 
 
 class Root():
     def __init__(self,title="Market Maker"):
         self.title = title
+        if 'VCAP_SERVICES' in os.environ:
+            self.host = "market-maker.frankencloud.net"
+        else:
+            self.host = "localhost:8080"
 
     def fetch(self):
-        return render_template('index.html', title=self.title)
+        return render_template('index.html', title=self.title, host=self.host)
--- library/_marketState.py
+++ library/_marketState.py
@@ -4,7 +4,6 @@ from flask import jsonify
 class State:
     def __init__(self, db):
         self.conn = db
-        self.conn['37d2890837d8dd6ec1006cbb8e450e5b'].fetch()
 
     def fetch(self):
         return jsonify(self.conn['37d2890837d8dd6ec1006cbb8e450e5b'])
